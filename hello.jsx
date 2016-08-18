d2f86b7094155ae3f39d2f1d6ac618ee3ce11a8e
Peng.Li <nowind_lee@qq.com>
2016-08-18 21:01:10 +0800

    Remove setTimeout

--- hello.jsx
+++ hello.jsx
@@ -6,11 +6,6 @@ class Hello extends Component {
     // required if we want to invoke `this.`
     super(props);
     this.state = {
-      // this is required,
-      // otherwise we will get an warning like
-      // Warning: Hello is changing an uncontrolled input of type text to be controlled. Input elements should not switch from uncontrolled to controlled (or vice versa). Decide between using a controlled or uncontrolled input element for the lifetime of the component.
-      username: '',
-      password: '',
       usernameError: '',
       passwordError: '',
       submitButtonEnabled: false
@@ -21,58 +16,46 @@ class Hello extends Component {
     return <form onSubmit={this._onSubmit.bind(this)}>
       <div>
         <input type="text"
-               onInput={this._onUsernameChanged.bind(this)}
+               onChange={this._onUsernameChanged.bind(this)}
                onBlur={this._checkUsername.bind(this)}
         />
         <span>{this.state.usernameError}</span>
       </div>
       <div>
         <input type="password"
-               onInput={this._onPasswordChange.bind(this)}
+               onChange={this._onPasswordChange.bind(this)}
                onBlur={this._checkPassword.bind(this)}
         />
         <span>{this.state.passwordError}</span>
       </div>
       <div>
-        <button type="submit" disabled={this.state.submitButtonEnabled ? '' : 'disabled'}>注册</button>
-      </div>
-      <hr />
-      <div>
-        <div>Username: {this.state.username}</div>
-        <div>Password: {this.state.password}</div>
+        <button type="submit" disabled={this.state.submitButtonEnabled ? '' : 'disabled'}>
+          注册
+        </button>
       </div>
     </form>;
   }
 
   _onUsernameChanged(event) {
-    const username = event.target.value;
     this.setState({
-      username: username,
+      username: event.target.value,
       usernameError: ''
-    });
-    setTimeout(()=> {
-      this._determineIfEnableSubmitButton();
-    });
+    }, () => this._determineIfEnableSubmitButton());
   }
 
   _checkUsername(event) {
-    var username = event.target.value;
-    if (checkUsername(username)) {
+    if (checkUsername(event.target.value)) {
       this.setState({usernameError: ''});
     } else {
-      this.setState({usernameError: '用户名不合法'});
+      this.setState({usernameError: '用户名长度必须>=6'});
     }
   }
 
   _onPasswordChange(event) {
-    let password = event.target.value;
     this.setState({
-      password: password,
+      password: event.target.value,
       passwordError: ''
-    });
-    setTimeout(()=> {
-      this._determineIfEnableSubmitButton();
-    });
+    }, () => this._determineIfEnableSubmitButton());
   }
 
   _checkPassword(event) {
@@ -80,13 +63,13 @@ class Hello extends Component {
     if (checkPassword(password)) {
       this.setState({passwordError: ''});
     } else {
-      this.setState({passwordError: '密码不合法'})
+      this.setState({passwordError: '密码长度必须>=10'})
     }
   }
 
   _onSubmit(event) {
     event.preventDefault();
-    alert('submitted!');
+    alert('will submit: ' + this.state.username + ', ' + this.state.password);
   }
 
   _determineIfEnableSubmitButton() {
